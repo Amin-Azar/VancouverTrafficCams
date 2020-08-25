2e99ff1ac366c4c0999b3f4bf49c13fef06bbae1
Amin Ghasemazar <aming@ece.ubc.ca>
2020-08-24 18:40:13 -0700

    downloading live images

--- analyze-image.py
+++ analyze-image.py
@@ -3,6 +3,8 @@ from utils.input_data import get_inpu_data
 from utils.sys_check import get_env_variables
 from utils.text_to_image import save_image
 import os
+import requests
+
 '''
 Computer Vision API (v3.0)
 https://westcentralus.dev.cognitive.microsoft.com/docs/services/computer-vision-v3-ga/operations/5d986960601faab4bf452005
@@ -16,9 +18,17 @@ remote_image_urls = get_inpu_data()
 cvc = computervision_client
 
 for i,img in enumerate(remote_image_urls):
-    #print(50*'-')
+
     name = os.path.basename(img).split('.')[0]
     print('Image,\t'+ str(i)+ ',\t' + str(name)+',')
+
+    # Download the image 
+    response = requests.get(img)
+    file = open("images/"+os.path.basename(img), "wb")
+    file.write(response.content)
+    file.close()
+
+    # Analyze and save the image
     s=''
     for f in [describe_image, categorize_image, tag_image, detect_color_image, get_landmark_image, extract_text_image]:
         s_tmp = f(cvc, img)
--- runAndUpdate.sh
+++ runAndUpdate.sh
@@ -5,7 +5,7 @@ do
     # grab new images and analyze
     python3 analyze-image.py
     # push to the repo/ github page
-    git add reports
+    git add reports images
     git commit -m "updated for $(date)"
     git push origin master
     # wait for next round
