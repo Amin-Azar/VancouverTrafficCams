5d40e1509d53fde6512ece8519295c921c1128e9
Amin Ghasemazar <aming@ece.ubc.ca>
2020-08-24 22:55:29 -0700

    update readme

--- utils/text_to_image.py
+++ utils/text_to_image.py
@@ -3,7 +3,7 @@ from PIL import Image, ImageDraw, ImageFont
 
 def save_image(txt, name, path='./reports/'):
     
-    img = Image.new('RGB', (420, 240), color = (50, 50, 50)) 
+    img = Image.new('RGB', (360, 240), color = (50, 50, 50)) 
     d = ImageDraw.Draw(img)
     d.text((10,10), txt, fill=(255,255,200))
     
