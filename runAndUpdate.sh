f32a6b1a77d2f0a0e76ea5fb6015f4ef78e525c4
Ubuntu <azureuser@cvfreevm18.b2apnbjjjxeu3e2mz0sopaut0h.bx.internal.cloudapp.net>
2020-08-26 02:45:04 +0000

    fixed sleep

--- runAndUpdate.sh
+++ runAndUpdate.sh
@@ -6,9 +6,10 @@ do
     python3 analyze-image.py
     python3 detection.py
     # push to the repo/ github page
+    git pull
     git add reports images images_out
     git commit -m "updated for $(date)"
     git push origin master
     # wait for next round
-    wait 3600 # every hour
+    sleep 3600 # every hour
 done
